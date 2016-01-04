16bd0a324fd302e73184409ae5448d603c45e17f
Pixelastic <tim@pixelastic.com>
2016-01-04 17:15:48 +0100

    test(config_loader): Add tests and sanity checks to config_loader
    I've added tests to the config_loader, as well as guessing some
    default values (`allowed_domains` from `start_urls`) and being more
    lenient on others (accepting string instead of array with one value).

---
+++ src/config_loader_test.py
@@ -0,0 +1,138 @@
+# coding: utf-8
+"""ConfigLoader tests"""
+from config_loader import ConfigLoader
+import pytest
+import os
+import json
+
+
+class TestInit:
+    def config(self, additional_config={}):
+        base_config = {
+            'allowed_domains': 'allowed_domains',
+            'api_key': 'api_key',
+            'app_id': 'app_id',
+            'custom_settings': 'custom_settings',
+            'hash_strategy': 'hash_strategy',
+            'index_name': 'index_name',
+            'index_prefix': 'index_prefix',
+            'selectors': 'selectors',
+            'selectors_exclude': 'selectors_exclude',
+            'start_urls': 'start_urls',
+            'stop_urls': 'stop_urls',
+            'strategy': 'strategy',
+            'strip_chars': 'strip_chars'
+        }
+        config = base_config.copy()
+        config.update(additional_config)
+        os.environ['CONFIG'] = json.dumps(config)
+
+
+    @staticmethod
+    def test_need_config_environment_variable():
+        """ Should throw if no CONFIG passed"""
+        # Given
+        os.environ['CONFIG'] = ''
+
+        # When / Then
+        with pytest.raises(SystemExit):
+            ConfigLoader()
+
+    @staticmethod
+    def test_need_json_env_var():
+        """ Should throw if CONFIG is not JSON """
+        # Given
+        os.environ['CONFIG'] = 'foobar'
+
+        # When / Then
+        with pytest.raises(ValueError):
+            ConfigLoader()
+
+    def test_mandatory_index_name(self):
+        """ Should throw if no index_name passed """
+        # Given
+        self.config({
+            'index_name': None
+        })
+
+        # When / Then
+        with pytest.raises(ValueError):
+            ConfigLoader()
+
+    def test_allowed_domains_accept_single_value(self):
+        """ Should allow passing allowed_domains as a string instead of an array
+        """
+        # Given
+        self.config({
+            'allowed_domains': 'www.foo.bar'
+        })
+
+        # When
+        actual = ConfigLoader()
+
+        # Then
+        assert actual.allowed_domains == ['www.foo.bar']
+
+    def test_mandatory_start_urls(self):
+        """ Should throw if no start_urls passed """
+        # Given
+        self.config({
+            'start_urls': None
+        })
+
+        # When / Then
+        with pytest.raises(ValueError):
+            ConfigLoader()
+
+    def test_start_urls_accept_single_value(self):
+        """ Allow passing start_urls as string instead of array """
+        # Given
+        self.config({
+            'start_urls': 'www.foo.bar'
+        })
+
+        # When
+        actual = ConfigLoader()
+
+        # Then
+        assert actual.start_urls == ['www.foo.bar']
+
+    def test_stop_urls_accept_single_value(self):
+        """ Allow passing stop_urls as string instead of array """
+        # Given
+        self.config({
+            'stop_urls': 'www.foo.bar'
+        })
+
+        # When
+        actual = ConfigLoader()
+
+        # Then
+        assert actual.stop_urls == ['www.foo.bar']
+
+
+    def test_start_urls_should_have_at_least_one_element(self):
+        """ Should throw if start_urls does not have at least one element """
+        # Given
+        self.config({
+            'start_urls': []
+        })
+
+        # When / Then
+        with pytest.raises(ValueError):
+            ConfigLoader()
+
+    def test_allowed_domains_uses_start_url_as_default(self):
+        """ Should use domain of start_urls for allowed_domains if none is set
+        """
+        # Given
+        self.config({
+            'start_urls': 'http://www.foo.bar/',
+            'allowed_domains': None
+        })
+
+        # When
+        actual = ConfigLoader()
+
+        # Then
+        assert actual.allowed_domains == ['www.foo.bar']
