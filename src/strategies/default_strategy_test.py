1da50dfadd727d6d547ae3ba5ae753665e9d09fa
Maxime Locqueville <maxiloc@gmail.com>
2016-02-10 12:37:32 +0100

    Fix default_value with empty content

--- src/strategies/default_strategy_test.py
+++ src/strategies/default_strategy_test.py
@@ -794,6 +794,76 @@ class TestGetRecordsFromDomWithDefaultValue:
         assert actual[0]['hierarchy']['lvl2'] is None
         assert actual[0]['content'] is None
 
+    def test_default_value_empty(self):
+        # Given
+        strategy = get_strategy({
+            'selectors': {
+                'lvl0': {
+                    'selector': 'h1',
+                    'default_value': 'Documentation'
+                },
+                'lvl1': 'h2',
+                'lvl2': 'h3',
+                'content': 'p'
+            }
+        })
+        strategy.dom = lxml.html.fromstring("""
+        <html><body>
+            <h1></h1>
+            <p>text</p>git
+            <h2>Bar</h2>
+            <h3>Baz</h3>
+        </body></html>
+        """)
+
+        # When
+        actual = strategy.get_records_from_dom()
+
+        # Then
+
+        # First record has the global H1
+        assert len(actual) == 4
+        assert actual[0]['type'] == 'lvl0'
+        assert actual[0]['hierarchy']['lvl0'] == 'Documentation'
+        assert actual[0]['hierarchy']['lvl1'] is None
+        assert actual[0]['hierarchy']['lvl2'] is None
+        assert actual[0]['content'] is None
+
+    def test_default_value_empty_and_global(self):
+        # Given
+        strategy = get_strategy({
+            'selectors': {
+                'lvl0': {
+                    'selector': 'h1',
+                    'global': 'true',
+                    'default_value': 'Documentation'
+                },
+                'lvl1': 'h2',
+                'lvl2': 'h3',
+                'content': 'p'
+            }
+        })
+        strategy.dom = lxml.html.fromstring("""
+        <html><body>
+            <p>text</p>
+            <h2>Bar</h2>
+            <h3>Baz</h3>
+            <h1></h1>
+        </body></html>
+        """)
+
+        # When
+        actual = strategy.get_records_from_dom()
+        # Then
+
+        # First record has the global H1
+        assert len(actual) == 4
+        assert actual[0]['type'] == 'content'
+        assert actual[0]['hierarchy']['lvl0'] == 'Documentation'
+        assert actual[0]['hierarchy']['lvl1'] is None
+        assert actual[0]['hierarchy']['lvl2'] is None
+        assert actual[0]['content'] == 'text'
+
 class TestGetRecordsFromDomWithStripChars:
     def test_strip_chars(self):
         # Given
