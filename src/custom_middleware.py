4205e3d39f0ede752769f0c98f93bbed9b4078f2
Maxime Locqueville <maxiloc@gmail.com>
2016-02-17 11:51:08 +0100

    Patch link processing to avoid crawling some stop urls. Add optimizely and keen config

--- src/custom_middleware.py
+++ src/custom_middleware.py
@@ -1,19 +1,20 @@
 """
-SeleniumMiddleware
+CustomMiddleware
 """
 
 from scrapy.http import Request, HtmlResponse
+from scrapy.exceptions import IgnoreRequest
 
 import json
 import time
 
-class SeleniumMiddleware(object):
+class CustomMiddleware(object):
     def __init__(self):
         self.seen = {}
-        self.driver = SeleniumMiddleware.driver
+        self.driver = CustomMiddleware.driver
 
     def process_request(self, request, spider):
-        # If the JS rendering is not needed
+
         if not spider.js_render:
             return None
 
@@ -34,5 +35,13 @@ class SeleniumMiddleware(object):
         )
 
     def process_response(self, request, response, spider):
+        # Since scrappy use start_urls and stop_urls before creating the request
+        # If the url get redirected then this url gets crawled even if it's not allowed to
+        # So we check if the final url is allowed
+        for rule in spider._rules:
+            if not rule.link_extractor._link_allowed(response) and not (spider.scrap_start_urls and response.url in spider.start_urls):
+                print "Ignored: " + response.url
+                raise IgnoreRequest()
+
         return response
 
