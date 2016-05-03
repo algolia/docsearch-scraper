7f1b28f035d10fc9a31b6c4a661b61ff6c6d3f58
Maxime Locqueville <maxiloc@gmail.com>
2016-05-03 23:07:35 +0200

    add a config bootstraper

---
+++ deployer/src/config_creator.py
@@ -0,0 +1,52 @@
+import tldextract
+import json
+import pyperclip
+from collections import OrderedDict
+
+import helpers
+
+config = OrderedDict((
+    ("index_name", ""),
+    ("start_urls", []),
+    ("stop_urls", []),
+    ("selectors", OrderedDict((
+        ("lvl0", "FIXME h1"),
+        ("lvl1", "FIXME h2"),
+        ("lvl2", "FIXME h3"),
+        ("lvl3", "FIXME h4"),
+        ("lvl4", "FIXME h5"),
+        ("text", "FIXME p, FIXME li")
+    )))
+))
+
+url = helpers.get_user_value("start url: ")
+
+config['index_name'] = tldextract.extract(url).domain
+
+if helpers.confirm("Does the start_urls require variables ?"):
+    config['start_urls'] = [{
+        "url": url + ('/' if url[-1] != '/' else '') + '(?P<static_variable>.*?)/(?P<dynamic_variable>.*?)/',
+        "variables": {
+            "static_variable": [
+              "value1",
+              "value2"
+            ],
+            "dynamic_variable": {
+              "url": url,
+              "js": "var versions = $('#selector option').map(function (i, elt) { return $(elt).html(); }).toArray(); return JSON.stringify(versions);"
+            }
+        }
+    }]
+else:
+    config['start_urls'] = [url]
+
+dump = json.dumps(config, separators=(',', ': '), indent=2)
+pyperclip.copy(dump)
+
+print ""
+print "============="
+print dump
+print "============="
+print ""
+print "Config copied to clipboard [OK]"
+print ""
