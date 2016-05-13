cd1c21236981e49864f119048fff387ce035b7cd
Léo Ercolanelli <leo.ercolanelli@gmail.com>
2016-05-13 17:38:14 +0200

    Create python 3 containers using --build-arg
    It is now possible to specify `--build-arg USE_PYTHON3=<true|false>`
    while building `Dockerfile` or `Dockerfile.dev` to make them use
    python 3.

--- cli/src/commands/abstract_command.py
+++ cli/src/commands/abstract_command.py
@@ -18,8 +18,12 @@ class AbstractCommand(object):
         return []
 
     @staticmethod
-    def build_docker_file(file, image="algolia/documentation-scrapper-dev"):
-        return AbstractCommand.exec_shell_command(["docker", "build", "-t", image, "-f", file, "."])
+    def build_docker_file(file, image="algolia/documentation-scrapper-dev", python3=False):
+        cmd = ["docker", "build", "-t", image, "-f", file]
+        if python3:
+            cmd.extend(['--build-arg', 'USE_PYTHON3=true'])
+        cmd.append('.')
+        return AbstractCommand.exec_shell_command(cmd)
 
     @staticmethod
     def exec_shell_command(arguments, env=None):
