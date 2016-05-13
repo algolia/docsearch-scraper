927d4d1439dd5b0e1f099286d7053868bc594a3b
Léo Ercolanelli <leo.ercolanelli@gmail.com>
2016-05-13 17:38:14 +0200

    Create python 3 containers using --build-arg
    It is now possible to specify `--build-arg USE_PYTHON3=<true|false>`
    while building `Dockerfile` or `Dockerfile.dev` to make them use
    python 3.

--- cli/src/commands/run_tests.py
+++ cli/src/commands/run_tests.py
@@ -1,5 +1,6 @@
 from abstract_command import AbstractCommand
 from os import getcwd
+from ..helpers import print_error
 
 class RunTests(AbstractCommand):
     def get_name(self):
@@ -8,8 +9,19 @@ class RunTests(AbstractCommand):
     def get_description(self):
         return 'Run tests'
 
+    def get_options(self):
+        return [{'name': 'python3', 'description': 'Build the docker image to use python 3 (true|false)'}]
+
     def run(self, args):
-        code = self.build_docker_file("scraper/dev/docker/Dockerfile.dev")
+        try:
+            py3 = args[0].lower()
+            py3[0] = args[0].upper()
+            py3 = bool(args[0])
+        except ValueError:
+            print_error("Invalid value for `python3': {}".format(args[0]))
+            return 1
+
+        code = self.build_docker_file("scraper/dev/docker/Dockerfile.dev", python3=py3)
         if code != 0:
             return code
 
