e33e232bec53a819a416510a928ed304b6265cbc
Sylvain Pace <sylvain.pace@algolia.com>
2018-05-30 11:56:09 +0200

    adding vuepress template

--- deployer/src/helpdesk_helper.py
+++ deployer/src/helpdesk_helper.py
@@ -138,6 +138,10 @@ def is_pkgdown_conversation(conversation):
     return "pkgdown" in conversation.get("tags")
 
 
+def is_vuepress_conversation(conversation):
+    return "vuepress" in conversation.get("tags")
+
+
 @rate_limited(200, 60)
 def search(query, page=1, pageSize=50, sortField="modifiedAt", sortOrder="asc"):
     search_endpoint = "https://api.helpscout.net/v1/search/conversations.json"
