e84c94c042b504f1931e04dba926cff8fcd25065
Pixelastic <tim@pixelastic.com>
2018-06-19 17:09:48 +0200

    Automatically adding to Analytics on deploy

--- deployer/src/algolia_internal_api.py
+++ deployer/src/algolia_internal_api.py
@@ -47,33 +47,59 @@ def get_indices_for_right(right):
     return []
 
 
-def add_user_to_index(config, email):
-    right = get_right_for_email(email)
+
+def add_user_to_index(index_name, user_email):
+    """Give a user access to the Analytics on the index
+
+    Args:
+        index_name: The name of the index
+        user_email: The email address to add
+
+    Returns:
+        - True if the user was successfully added
+        - None if the user was already given access to this index
+        - {url} a string pointing to an invitation link if the user does not
+          yet have an Algolia account
+    """
+
+    right = get_right_for_email(user_email)
     indices = get_indices_for_right(right)
 
-    if config in indices:
-        return
+    # User is already added to this index
+    if index_name in indices:
+        return None;
 
-    indices.append(config)
+    indices.append(index_name)
 
+    payload = {
+        'application_right': {
+            'application_id': 13240,  # website internal docsearch app id
+            'user_email': user_email,
+            'indices': indices,
+            'analytics': True
+        }
+    }
+    headers = get_headers();
+
+
+    # User has already access to some other indicies
     if right:
-        requests.patch(get_endpoint('/application_rights/' + str(right['id'])), json={
-            'application_right': {
-                'application_id': 13240,  # website internal docsearch app id
-                'user_email': email,
-                'indices': indices,
-                'analytics': True
-            }
-        }, headers=get_headers())
-    else:
-        requests.post(get_endpoint('/application_rights'), json={
-            'application_right': {
-                'application_id': 13240, # website internal docsearch app id
-                'user_email': email,
-                'indices': indices,
-                'analytics': True
-            }
-        }, headers=get_headers())
+        endpoint = get_endpoint('/application_rights/' + str(right['id']))
+        requests.patch(endpoint, json=payload, headers=headers)
+        return True
+
+    # Adding user for the first time
+    endpoint = get_endpoint('/application_rights/')
+    response = requests.post(endpoint, json=payload, headers=headers)
+    data = response.json()
+    invitation_url = data['user']['invitation_url']
+
+    # User does not have an Algolia account, they must follow this invitation url
+    if invitation_url:
+        return invitation_url
+    
+    # User has an Algolia account, they have been added to the index
+    return True
 
 def remove_user_from_index(config, email):
     right = get_right_for_email(email)
