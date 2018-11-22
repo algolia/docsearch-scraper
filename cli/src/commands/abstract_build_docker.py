5d99e7b38d7812a52f9f02a491692c91bf03c2cb
s-pace <sylvain.pace@algolia.com>
2018-11-22 08:45:15 +0100

    removing unused option

--- cli/src/commands/abstract_build_docker.py
+++ cli/src/commands/abstract_build_docker.py
@@ -2,16 +2,6 @@ from .abstract_command import AbstractCommand
 
 
 class AbstractBuildDocker(AbstractCommand):
-    def get_options(self):
-        return [{'name': 'python3',
-                 'description': 'Build the docker image to use python 3 (true|false)',
-                 'optional': 'false'}]
-
-    @staticmethod
-    def python3_parse(arg):
-        if isinstance(arg, bool):
-            return arg
-        return isinstance(arg, str) and arg.lower() == 'true'
 
     @staticmethod
     def build_docker_file(file, image="algolia/documentation-scraper-dev"):
