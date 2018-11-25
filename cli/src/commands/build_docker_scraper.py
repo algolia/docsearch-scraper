33378e076b2b363f5d3ef65c69b436bcb66c59fc
s-pace <sylvain.pace@algolia.com>
2018-11-25 11:39:26 +0100

    use test from docker image

--- cli/src/commands/build_docker_scraper.py
+++ cli/src/commands/build_docker_scraper.py
@@ -8,10 +8,29 @@ class BuildDockerScraper(AbstractBuildDocker):
     def get_description(self):
         return 'Build scraper images (dev, prod)'
 
+    def get_options(self):
+        return [{"name": "test",
+                 "description": "build the test image",
+                 'optional': False}]
+
+    @staticmethod
+    def test_parse(args):
+        if len(args) < 2:
+            return False
+        if isinstance(args[1], bool):
+            return args[1]
+        return isinstance(args[1], str) and args[1].lower() == 'true'
+
     def run(self, args):
 
         code = self.build_docker_file("scraper/dev/docker/Dockerfile.base",
                                       "algolia/base-docsearch-scraper")
+
+        test = self.get_option('test', args)
+
+        if test:
+            return self.build_docker_file("scraper/dev/docker/Dockerfile.test",
+                                          "algolia/docsearch-scraper-test")
         if code != 0:
             return code
         code = self.build_docker_file("scraper/dev/docker/Dockerfile.dev")
--- cli/src/commands/run_tests.py
+++ cli/src/commands/run_tests.py
@@ -8,5 +8,34 @@ class RunTests(AbstractCommand):
     def get_description(self):
         return "Run tests"
 
+    def get_options(self):
+        return [{"name": "docker",
+                 "description": "run test from docker image",
+                 'optional': False}]
+
+    @staticmethod
+    def docker_parse(args):
+        if len(args) < 2:
+            return False
+        if isinstance(args[1], bool):
+            return args[1]
+        return isinstance(args[1], str) and args[1].lower() == 'true'
+
     def run(self, args):
+
+        docker = self.get_option('docker', args)
+
+        if docker:
+            self.exec_shell_command(["./docsearch", "docker:build", "true"])
+            run_command = [
+                "docker",
+                "run",
+                "--rm",
+                "-i",
+                "--name",
+                "docsearch-scraper-test",
+                "-t",
+                "algolia/docsearch-scraper-test"]
+            return self.exec_shell_command(run_command)
+
         return self.exec_shell_command(["pytest", "./scraper/src"])
