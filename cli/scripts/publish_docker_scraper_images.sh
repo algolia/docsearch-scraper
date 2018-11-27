80db5415e07780779d3850c244cfe56282d9df4e
s-pace <sylvain.pace@algolia.com>
2018-11-27 10:04:20 +0100

    deployng current branch, not only master

--- cli/scripts/publish_docker_scraper_images.sh
+++ cli/scripts/publish_docker_scraper_images.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-
-git checkout master || exit 1
-git pull origin master
-
 ./docsearch docker:build
 
 docker push algolia/docsearch-scrapper-base:latest
