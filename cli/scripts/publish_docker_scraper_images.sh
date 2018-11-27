e988f4d1f443431d1a2bae9395a33d3b9dedd4c8
s-pace <sylvain.pace@algolia.com>
2018-11-27 10:09:08 +0100

    proper renaming

--- cli/scripts/publish_docker_scraper_images.sh
+++ cli/scripts/publish_docker_scraper_images.sh
@@ -2,7 +2,7 @@
 
 ./docsearch docker:build
 
-docker push algolia/docsearch-scrapper-base:latest
-docker push algolia/docsearch-scrapper:latest
+docker push algolia/docsearch-scraper-base:latest
+docker push algolia/docsearch-scraper:latest
 
 git checkout $current_branch
