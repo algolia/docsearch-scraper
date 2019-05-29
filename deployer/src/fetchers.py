2553ca4869150472f0dc6e13f616f3fc092d2e54
s-pace <sylvain.pace@algolia.com>
2019-05-29 10:46:54 +0200

    fix(fetcher): get only tracked config by git

--- deployer/src/fetchers.py
+++ deployer/src/fetchers.py
@@ -2,12 +2,17 @@ import os
 import json
 from collections import OrderedDict
 from os import environ
+from . import helpers
 
 
 def get_configs_from_repos():
     configs = {}
     public_dir = environ.get('PUBLIC_CONFIG_FOLDER')
     private_dir = environ.get('PRIVATE_CONFIG_FOLDER')
+
+    tracked_configs = helpers.check_output_decoded(
+        ['git', 'ls-tree', '-r', 'master', '--name-only'],
+        cwd=public_dir).splitlines()
 
     if public_dir is None or private_dir is None:
         print(
@@ -25,7 +30,10 @@ def get_configs_from_repos():
                 with open(path, 'r') as f:
                     txt = f.read()
                 config = json.loads(txt, object_pairs_hook=OrderedDict)
-                configs[config['index_name']] = config
+                if 'configs/{}.json'.format(
+                        config['index_name']) in tracked_configs:
+                    configs[config['index_name']] = config
+
     print('{} docs in public and private repo'.format(len(configs)))
 
     return configs
