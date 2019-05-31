f7dd6ea1b52207f267f4568448f98f77277c7e5a
Benoit Perrot <benoit.perrot@algolia.com>
2019-05-31 10:04:54 +0200

    scrapper: JsExecutor: Declare "driver" class variable

--- scraper/src/js_executor.py
+++ scraper/src/js_executor.py
@@ -3,9 +3,12 @@ import json
 
 
 class JsExecutor:
+    driver = None
+
     def __init__(self):
         self.driver = JsExecutor.driver
 
+    # TODO: find out why JsExecutor.driver couldn't be used
     def execute(self, url, js):
         self.driver.get(url)
         time.sleep(5)
