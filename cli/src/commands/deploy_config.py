fbb9d17dc37fb7f653801a3562a45f7cf8841df6
Robert Mogos <robert.mogos@algolia.com>
2020-09-23 15:34:37 +0200

    fix: add config_exists to the command in order to add a new api key

--- cli/src/commands/deploy_config.py
+++ cli/src/commands/deploy_config.py
@@ -11,6 +11,7 @@ class DeployConfig(AbstractCommand):
     def get_options(self):
         return [{"name": "index_name",
                  "description": "name of the config to deploy"},
+                 {"name": "config_exists", "description": "the config is already in the repo"},
                  {"name": "push_config", "description": "push the config to master", "optional": True}]
 
     def run(self, args):
@@ -20,4 +21,4 @@ class DeployConfig(AbstractCommand):
             exit(1)
         else:
             from deployer.src.index import deploy_config
-            deploy_config(args[0], args[1] if len(args) > 1 else True)
+            deploy_config(args[0], args[1], args[2] if len(args) > 2 else True)
