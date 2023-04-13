a32b3031e13fab505485b6eff25574943f8b967f
erichiggins0 <erichiggins@applied.co>
2023-04-12 20:34:10 -0700

    add --disable-dev-shm-usage

--- scraper/src/config/browser_handler.py
+++ scraper/src/config/browser_handler.py
@@ -24,6 +24,7 @@ class BrowserHandler:
             chrome_options = Options()
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('user-agent={0}'.format(user_agent))
             chrome_options.binary_location = os.environ.get("CHROME_PATH", "/usr/bin/google-chrome")
 
